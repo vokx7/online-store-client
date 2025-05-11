e2c859a4e789b8bf63909cf0069ab2332e07cb4d
vokx7 <vokx77@gmail.com>
2025-05-11 13:08:01 +0200

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/online-store-client/",
   server: {
     port: 3000,
   },
